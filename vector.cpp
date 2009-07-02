9387e5222ad106d5c329bd3d88a77dc1051e29f8
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2009-07-02 10:25:46 -0500

    add fill and vector::resize function calls

--- vector.cpp
+++ vector.cpp
@@ -6,19 +6,23 @@ using namespace std;
 
 int main ()
 {
-  unsigned int i=5;
+  unsigned int i;
+  unsigned int size=100000000;
   vector<int> myvector;
 
-  cout << "myvector.reserve("<<i<<")"<<endl;
+  cout << "myvector.reserve("<<size<<")"<<endl;
+  cout << "myvector.reserve "<<sizeof(int)*i/1024/1024<<" MByte"<<endl;
   myvector.reserve(i);
   cout << "size: " << (int) myvector.size() << "\n";
   cout << "capacity: " << (int) myvector.capacity() << "\n";
   cout << "max_size: " << (int) myvector.max_size() << "\n";
 
   cout << "myvector.push_back() 3 times"<<endl;
-  myvector.push_back (100);
-  myvector.push_back (200);
-  myvector.push_back (300);
+  myvector.push_back (0);
+  myvector.push_back (127);
+  myvector.push_back (255);
+
+  for (i=0; i<myvector.size(); i++) cout << " " << myvector[i];
   cout << "size: " << (int) myvector.size() << "\n";
   cout << "capacity: " << (int) myvector.capacity() << "\n";
   cout << "max_size: " << (int) myvector.max_size() << "\n";
@@ -26,6 +30,7 @@ int main ()
   cout << "myvector contains:";
   for (i=0; i<myvector.size(); i++) cout << " " << myvector[i];
 
+//clear
   cout <<endl<< "myvector.clear()"<<endl;
   myvector.clear();
   cout << "size: " << (int) myvector.size() << "\n";
@@ -33,8 +38,8 @@ int main ()
   cout << "max_size: " << (int) myvector.max_size() << "\n";
 
   cout << "myvector.push_back() 2 times"<<endl;
-  myvector.push_back (1101);
-  myvector.push_back (2202);
+  myvector.push_back (255);
+  myvector.push_back (0);
   cout << "size: " << (int) myvector.size() << "\n";
   cout << "capacity: " << (int) myvector.capacity() << "\n";
   cout << "max_size: " << (int) myvector.max_size() << "\n";
@@ -42,7 +47,18 @@ int main ()
   cout << "\nmyvector contains:";
   for (i=0; i<myvector.size(); i++) cout << " " << myvector[i];
 
-  cout << endl;
+//fill
+  cout <<endl<< "myvector.resize("<<size<<")"<<endl;
+  myvector.resize(size);
+  fill(myvector.begin()+3,myvector.end(),1);
+  cout << "size: " << (int) myvector.size() << "\n";
+  cout << "capacity: " << (int) myvector.capacity() << "\n";
+  cout << "max_size: " << (int) myvector.max_size() << "\n";
+
+  for (i=0; i<3; i++) cout << " " << myvector[i];
+  cout <<"...";
+  for (i=myvector.size()-3;i<myvector.size(); i++) cout << " " << myvector[i];
+  cout <<endl;
 
   return 0;
 }
