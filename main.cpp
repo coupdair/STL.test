9350d648ed66b17508d4e38bc6b3a5aeb2241a8b
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-04-08 11:10:48 +0200

    main test: add error message if any command line argument modified:   main.cpp: add message on error output to stop the program execution (i.e. return) if any argument has been added to command line of the program call (i.e. shell call)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //main test
 //compilation line: g++ -o main main.cpp
 //run line: ./main
-//shell line: g++ -o main main.cpp && ./main
+//shell line: g++ -o main main.cpp && ./main && ./main 123 && ./main 123 > /dev/null
 #include <iostream>
 
 //using namespace std;
@@ -10,6 +10,15 @@ int main(int argc, char *argv[])
 {
   //put binary name to standard output
   std::cout<<"binary name: "<<argv[0]<<std::endl;
+
+  //example of program stop in case of 1 command line argument set by user (e.g. ./main hop)
+  if(argc==2)
+  {//return not zero value to specify the error (i.e. error code value)
+    std::cerr<<"error: no argument is used by this program."<<std::endl;
+    return 1;
+  }
+
+  //return 
   return 0;
 }
 
