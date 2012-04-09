80e3c1a1ae9fbc1a847e65ab5835e703f2baa0f5
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-04-09 14:06:47 +0200

    time: system_time class modified:   time.cpp: create a class to use timeval and gettimeofday

--- time.cpp
+++ time.cpp
@@ -2,7 +2,7 @@
 //compilation line: g++ -o time time.cpp
 //run line: ./time
 //shell line: g++ -o time time.cpp && ./time
-//! \todo make a class
+//! \todo v make a class
 //! \todo option: use POSIX
 //! \todo option: use boost::
 //! \todo option: use mpi::
@@ -11,21 +11,45 @@
 
 //using namespace std;
 
+class system_time
+{
+//data members
+  //start and stop dates
+  struct timeval oldStamp, newStamp;
+  //elapsed time
+  long time;
+//function members
+  public:
+  //get start time
+  void start(void)
+  {//get start date
+    gettimeofday(&oldStamp,NULL);
+  }
+  //get stop date and compute elapsed time
+  void stop(void)
+  {
+    //get stop date
+    gettimeofday(&newStamp,NULL);
+    //compute time delay
+    time=newStamp.tv_sec-oldStamp.tv_sec;//time in second
+    time=newStamp.tv_usec-oldStamp.tv_usec+1000000*time;//time in micro second
+  }
+  //get elapsed time
+  int elapsed_time(void)
+  {
+    return time;
+  }
+};
+
 int main(int argc, char *argv[])
 {
-  //dates
-  static struct timeval oldStamp, newStamp;
-
-  gettimeofday(&oldStamp,NULL);//start time
+  //elapsed time
+  system_time time;
+  time.start();//get start time
   //put binary name to standard output
   std::cout<<"binary name: "<<argv[0]<<std::endl;
-  gettimeofday(&newStamp,NULL);//stop  time
-
-  long time;//time
-  time=newStamp.tv_sec-oldStamp.tv_sec;//time in second
-  time=newStamp.tv_usec-oldStamp.tv_usec+1000000*time;//time in micro second
-
-  std::cout<<"time="<<time<<" us"<<std::endl;
+  time.stop();//get stop time
+  std::cout<<"time="<<time.elapsed_time()<<" us"<<std::endl;//get elapsed time
 
   //return 
   return 0;
