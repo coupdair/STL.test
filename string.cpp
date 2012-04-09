0de5f38a6db33870c0136cee1792a823c0fe7a84
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-04-09 15:50:10 +0200

    string: add many function and operator examples modified:   string.cpp: add cat (i.e. +), reserve (and also size, capacity, max_size), push_back, character access (i.e. []) and finaly clear functions (or operator).

--- string.cpp
+++ string.cpp
@@ -1,5 +1,8 @@
 //STL::string test
 //from http://www.cplusplus.com/reference/stl/
+//compilation line: g++ -o string string.cpp
+//run line: ./string
+//shell line: g++ -o string string.cpp && ./string && ./string 123
 #include <iostream>
 
 #include <string>
@@ -21,62 +24,45 @@ int main(int argc, char *argv[])
   }
 
 //standard output
-  std::cout<<"str=\""<<str<<"\""<<std::endl;
+  std::cout<<"str=\""<<str<<"\"."<<std::endl;
 
-/*
+//cat
+  std::cerr<<"* cat string or char (i.e. + operator):"<<std::endl;
+  str="basename_"+str+".ext";
+  std::cout<<"str =\""<<str<<"\"."<<std::endl;
+
 //reserve
-  cout << "myvector.reserve("<<size<<")"<<endl;
-  cout << "myvector.reserve "<<sizeof(int)*size/1024/1024<<" MByte"<<endl;
-  myvector.reserve(size);
-  cout << "size: " << (int) myvector.size() << "\n";
-  cout << "capacity: " << (int) myvector.capacity() << "\n";
-  cout << "max_size: " << (int) myvector.max_size() << "\n";
+  std::cerr<<"* reserve function:"<<std::endl;
+  std::cout << "defaults"<<std::endl;
+  std::cout << "  size: "     << (int) str.size() << "\n";
+  std::cout << "  capacity: " << (int) str.capacity() << "\n";
+  std::cout << "  max_size: " << (int) str.max_size() << "\n";
+  int size=128;
+  std::cout << "str.reserve("<<size<<")"<<std::endl;
+  str.reserve(size);
+  std::cout << "  size: "     << (int) str.size() << "\n";
+  std::cout << "  capacity: " << (int) str.capacity() << "\n";
+  std::cout << "  max_size: " << (int) str.max_size() << "\n";
 
 //push_back
-  cout << "myvector.push_back() 3 times"<<endl;
-  myvector.push_back (0);
-  myvector.push_back (127);
-  myvector.push_back (255);
+  std::cerr<<"* push_back function:"<<std::endl;
+  for(int i=0;i<5; i++) str.push_back('0'+i);
+  std::cout<<"str =\""<<str<<"\"."<<std::endl;
 
-  for (i=0; i<myvector.size(); i++) cout << " " << myvector[i];
-  cout <<endl;
-  cout << "size: " << (int) myvector.size() << "\n";
-  cout << "capacity: " << (int) myvector.capacity() << "\n";
-  cout << "max_size: " << (int) myvector.max_size() << "\n";
-
-  cout << "myvector contains:";
-  for (i=0; i<myvector.size(); i++) cout << " " << myvector[i];
-  cout <<endl;
+//char
+  std::cerr<<"* [] operator:"<<std::endl;
+  std::cout<<"str["<<str.size()<<"]=";
+  for(int i=0;i<str.size();i++) std::cout<<"'"<<str[i]<<"' ";
+  std::cout<<std::endl;
 
 //clear
-  cout << "myvector.clear()"<<endl;
-  myvector.clear();
-  cout << "size: " << (int) myvector.size() << "\n";
-  cout << "capacity: " << (int) myvector.capacity() << "\n";
-  cout << "max_size: " << (int) myvector.max_size() << "\n";
+  std::cerr<<"* clear function:"<<std::endl;
+  std::cout << "str.clear()"<<std::endl;
+  str.clear();
+  std::cout << "  size: "     << (int) str.size() << "\n";
+  std::cout << "  capacity: " << (int) str.capacity() << "\n";
+  std::cout << "  max_size: " << (int) str.max_size() << "\n";
 
-  cout << "myvector.push_back() 2 times"<<endl;
-  myvector.push_back (255);
-  myvector.push_back (0);
-  cout << "size: " << (int) myvector.size() << "\n";
-  cout << "capacity: " << (int) myvector.capacity() << "\n";
-  cout << "max_size: " << (int) myvector.max_size() << "\n";
-
-  cout << "\nmyvector contains:";
-  for (i=0; i<myvector.size(); i++) cout << " " << myvector[i];
-
-//fill
-  cout <<endl<< "myvector.resize("<<size<<")"<<endl;
-  myvector.resize(size);
-  fill(myvector.begin()+3,myvector.end(),1);
-  cout << "size: " << (int) myvector.size() << "\n";
-  cout << "capacity: " << (int) myvector.capacity() << "\n";
-  cout << "max_size: " << (int) myvector.max_size() << "\n";
-
-  for (i=0; i<range; i++) cout << " " << myvector[i];
-  cout <<"...";
-  for (i=myvector.size()-range;i<myvector.size(); i++) cout << " " << myvector[i];
-  cout <<endl;
-*/
   return 0;
 }
+
