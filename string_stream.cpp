5b66823e8ee55e6f5bcae87bc334473ea2c8876f
Sebastien COUDERT <sebastien.coudert@univ-lille1.fr>
2012-04-09 15:08:10 +0200

    string_stream: add valueToString function (WiP) modified:   string_stream.cpp: add valueToString function (need to reserve before filling !!)

--- string_stream.cpp
+++ string_stream.cpp
@@ -10,45 +10,59 @@
 unsigned int stringToValue(const std::string &s)
 //template<typename T> T stringToValue(const std::string &s)
 {
-     std::istringstream stream(s);
-     unsigned int t;
+  std::istringstream stream(s);
+  unsigned int t;
 //     T t;
-     stream >> t;
-     return t;
-}
-/*
-template<typename T> std::string valueToString(const T& t)
+  stream >> t;
+  return t;
+}//stringToValue
+
+//! convert value to string (might be fixed size with specific character fill)
+/**
+ * format value to string, string size can be fixed and filled with a specific character
+**/
+template<typename T> std::string valueToString(const T& value,int size=-1,char c='0')
 {
-     std::ostringstream stream;
-     stream << t;
-     return stream.str();
-}
-*/
+  std::ostringstream stream;
+  if(size>0)
+  {
+    //stream.reserve(zeros);
+//stream<<"0123456879";
+stream<<"0000000000";
+    stream.fill(c);
+  }
+  stream << value;
+  return stream.str();
+}//valueToString
+
 //using namespace std;
 
 int main(int argc, char *argv[])
 {
 //string to value
-  int size=123;
+  int value=123;
   std::string str("default");
   if(argc==2)
   {
     str=argv[1];
-    size=(int)stringToValue(str);
+    std::cerr<<"* command line argument from stringToValue function:"<<std::endl;
+    value=(int)stringToValue(str);
   }
-  std::cout<<"size="<<size<<"\n"<<std::flush;
-  std::cout<<"str=\""<<str<<"\"."<<std::endl;
+  std::cout<<"value="<<value<<"\n"<<std::flush;
+  std::cout<<"str =\""<<str<<"\"."<<std::endl;
 
-/*
 //value to string
-
-  std::cout<<"str=\""<<str<<"\"."<<std::endl;
+  std::cerr<<"* valueToString function:"<<std::endl;
+  str=valueToString(value);
+  std::cout<<"str =\""<<str<<"\"."<<std::endl;
 
 //value to string (fill with)
+  std::cerr<<"* valueToString function:"<<std::endl;
   int zeros=10;
-  std::cout<<"output number using "<<zeros<<" character(s) filling with 0."<<std::endl;
-  std::cout<<"str=\""<<str<<"\"."<<std::endl;
-*/
+  str=valueToString(value,zeros);
+  std::cerr<<"output number using "<<zeros<<" character(s) filling with 0."<<std::endl;
+  std::cout<<"str =\""<<str<<"\"."<<std::endl;
 
   return 0;
 }
+
